0e638eb50a675d78fa21a9e02688bce9ef7ce32a
nagarajuthandu <nagaraju.thandu@cmritonline.ac.in>
2022-05-20 18:30:24 +0530

    deploy1

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/Angular_Client'));
+ngApp.use(express.static('./dist/client'));
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/Angular_Client/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/client/index.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
