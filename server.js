ae9a1a9c8aa28e88bd5f9541e6587b178d22e4a1
nagarajuthandu <nagaraju.thandu@cmritonline.ac.in>
2022-05-20 15:43:55 +0530

    heroku deployment proct

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ ngApp.use(express.static('./dist/Angular_Client'));
 ngApp.get('/*', function (request, response) {
     response.sendFile(path.join(__dirname, '/dist/Angular_Client/index.html'));
 });
-ngApp.listen(process.env.PORT || 80);
+ngApp.listen(process.env.PORT || 8080);
